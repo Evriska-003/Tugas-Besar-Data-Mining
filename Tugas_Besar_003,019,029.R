6c85737cd16536784681934c70f48d25cbf336d4
Evriska Dayanti <76762953+Evriska-003@users.noreply.github.com>
2021-01-18 12:51:23 +0700

    Update Tugas_Besar_003,019,029.R

--- Tugas_Besar_003,019,029.R
+++ Tugas_Besar_003,019,029.R
@@ -33,9 +33,12 @@ datatesting <- dataset[,1:6]
 
 #prediksi
 predictions <- predict(model, datatesting)
-
+head(predictions)
 #membandingkan hasil prediksi dari datatesting dengan dataset
 table(predictions, dataset$Result_of_Treatment)
+
+#Melihat tingkat akurasi
+mean(predictions  == dataset$Result_of_Treatment)
 
 #Memngetahui rule model
 rulemodel <- C5.0(Result_of_Treatment ~., data = dataset, rules = TRUE)
